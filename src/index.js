e643c713aab6f377f1bc08efb5969189a377a22b
Antonio Ochoa <antonhub00@gmail.com>
2021-10-07 00:30:09 -0500

    Movie view-model and "add movie" view added in entry point

--- src/index.js
+++ src/index.js
@@ -1,2 +1,10 @@
 import "bootstrap"; // Bootstrap JS
 import "./styles/index.scss"; // Bootstrap CSS
+import MovieViewModel from "./js/movieComponents/MovieViewModel";
+import AddMovieView from "./js/movieComponents/AddMovieView";
+
+const movieViewModel = new MovieViewModel();
+
+const addMovieView = new AddMovieView(movieViewModel);
+
+movieViewModel.attach(addMovieView);
